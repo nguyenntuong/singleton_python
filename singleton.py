981ceee7933334ff0b49581ac6749715ba4babf9
Tuong Nguyen <nguyenntuong@gmail.com>
2019-09-18 04:02:46 +0700

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -4,16 +4,16 @@ __singleton_instances_mn = ReadOnlyDict()
 #Python decorator for singleton pattern
 def singleton(_class):
     global __singleton_instances_mn
-    if _class.__name__ not in __singleton_instances_mn:
+    if _class.__qualname__ not in __singleton_instances_mn:
         def get_singleton(*args,**kargs):
             # Multithread locker
             locker = threading.Lock()
             locker.acquire()
-            if _class.__name__ not in __singleton_instances_mn:
-                __singleton_instances_mn[_class.__name__] = _class(*args,**kargs)
+            if _class.__qualname__ not in __singleton_instances_mn:
+                __singleton_instances_mn[_class.__qualname__] = _class(*args,**kargs)
             locker.release()
-            return __singleton_instances_mn[_class.__name__]
+            return __singleton_instances_mn[_class.__qualname__]
     else:
         def get_singleton():
-            return __singleton_instances_mn[_class.__name__]
+            return __singleton_instances_mn[_class.__qualname__]
     return get_singleton
